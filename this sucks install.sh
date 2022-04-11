15d586afdbaf8cb2e3a2136a788ed01105230862
James <james@james-wood.com.au>
2022-04-11 12:46:20 +1000

    Update this sucks install.sh

--- this sucks install.sh
+++ this sucks install.sh
@@ -5,6 +5,7 @@ sudo ln -s ${pwd}/URL /etc/xdg/openbox/environment
 ln -s ${pwd}/bash_profile ~/.bash_profile
 
 sudo ln -s ${pwd}/Autobright.service /etc/systemd/system/Autobright.service
+pip3 install rpi-backlight
 sudo systemctl enable Autobright
 sudo systemctl start Autobright
 echo "Now reboot"
