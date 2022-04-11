4f35b1518c933d7f6a363fa7731588298c3536a7
James <james@james-wood.com.au>
2022-04-11 12:45:41 +1000

    Update this sucks install.sh

--- this sucks install.sh
+++ this sucks install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox chromium-browser
+sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox chromium-browser python3-pip
 sudo ln -s ${pwd}/autostart /etc/xdg/openbox/autostart
 sudo ln -s ${pwd}/URL /etc/xdg/openbox/environment
 ln -s ${pwd}/bash_profile ~/.bash_profile
